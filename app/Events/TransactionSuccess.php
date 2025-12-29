8e5408b619c3eda08b2e99f3dfefd3f94697f498
Lemanh <lemanh.laravel@gmail.com>
2025-12-30 00:59:46 +0700

    up

--- app/Events/TransactionSuccess.php
+++ app/Events/TransactionSuccess.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
