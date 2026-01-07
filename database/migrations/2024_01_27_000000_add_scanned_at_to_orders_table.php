481ce1a60779cff6654117e0f93fc74f7481aaee
lemanh <lemanh.laravel@gmail.com>
2026-01-07 16:34:14 +0700

    up

--- database/migrations/2024_01_27_000000_add_scanned_at_to_orders_table.php
+++ database/migrations/2024_01_27_000000_add_scanned_at_to_orders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::table('orders', function (Blueprint $table) {
             $table->timestamp('scanned_at')->nullable()->after('updated_at');
             $table->string('old_scanned_status', 20)->nullable()->after('scanned_at'); // Lưu status lần scan trước
-            $table->index(['updated_at', 'scanned_at'], 'orders_scanned_index');
+            $table->index(['updated_at', 'scanned_at']);
         });
     }
 
@@ -24,7 +24,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->dropIndex('orders_scanned_index');
+            // $table->dropIndex('orders_scanned_index');
             $table->dropColumn(['scanned_at', 'old_scanned_status']);
         });
     }
