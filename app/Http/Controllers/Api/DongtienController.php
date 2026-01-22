eb93f88dfe3c911cf435f5f9921cbd9d5a7a47c6
lemanh <lemanh.laravel@gmail.com>
2026-01-22 16:59:36 +0700

    up

--- app/Http/Controllers/Api/DongtienController.php
+++ app/Http/Controllers/Api/DongtienController.php
@@ -27,7 +27,7 @@ class DongtienController extends Controller
         $query->orderBy('id', 'desc');
 
         // Phân trang
-        $perPage = $request->get('per_page', 5);
+        $perPage = $request->get('per_page', 10);
         $transactions = $query->paginate($perPage);
 
         return response()->json($transactions);
