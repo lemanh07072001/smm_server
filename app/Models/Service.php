7632c64238c9502513e8ef91ce7d257f85332b08
lemanh <lemanh.laravel@gmail.com>
2026-02-27 15:19:56 +0700

    up

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -56,6 +56,7 @@ class Service extends Model
                 'tiktok_comment_livestream'                 => 'Comment Tiktok Livestream',
                 'tiktok_share'                              => 'Share Tiktok',
                 'tiktok_buff_view_live'                     => 'Buff View Live',
+                'tiktok_buff_view_video'                    => 'Buff View Video',
             ]
         ],
         '3' => [
