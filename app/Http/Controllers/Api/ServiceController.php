7780d59e0b915a4581b7236f8e885c0733dd0a60
lemanh <lemanh.laravel@gmail.com>
2026-03-03 09:56:23 +0700

    up

--- app/Http/Controllers/Api/ServiceController.php
+++ app/Http/Controllers/Api/ServiceController.php
@@ -10,6 +10,7 @@ use App\Models\Service;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 
 class ServiceController extends Controller
 {
